f677a2b040a6d71fadcd5ac38ba61497de5444ef
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2019-12-15 05:15:42 +0200

    0.0.1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,44 +1,42 @@
 <template>
-<div>
-    <footer class="text-gray-500 bg-black sm:rounded-xl">
-      <section class="border-b border-gray-600 border-solid md:ml-24">
-        <h4 class="pt-16 mx-8 mb-4 text-xs font-semibold text-red-700 uppercase">Fine dining at its best</h4>
-        <h1 class="mx-8 mb-6 text-gray-100 text-md">Join us at the best fine dining restaurant in Oudtshoorn & the Klein Karoo.</h1>
-        <h3 class="mx-8 mr-6 text-sm">Experience an evening you'll never forget at the Black Swan Restaurant & Wine Bar. Bring a loved one or a few friends and let us be a part of your wonderful memories.</h3>
-        <g-link to="/reservetable"><button class="px-6 py-3 mx-8 my-12 font-semibold text-gray-100 bg-red-700 rounded-xl focus:outline-none">Make A Reservation</button></g-link>
+  <footer class="text-gray-500 bg-black md:rounded-xl md:text-center md:mb-16">
+    <section class="border-b border-gray-600 border-solid md:ml-24">
+      <h4 class="pt-16 mx-8 mb-4 text-xs font-semibold text-red-700 uppercase">Fine dining at its best</h4>
+      <h1 class="mx-8 mb-6 text-gray-100 text-md">Join us at the best fine dining restaurant in Oudtshoorn & the Klein Karoo.</h1>
+      <h3 class="mx-8 mr-6 text-sm">Experience an evening you'll never forget at the Black Swan Restaurant & Wine Bar. Bring a loved one or a few friends and let us be a part of your wonderful memories.</h3>
+      <g-link to="/reservetable"><button class="px-6 py-3 mx-8 my-12 font-semibold text-gray-100 bg-red-700 rounded-xl focus:outline-none">Make A Reservation</button></g-link>
+    </section>
+    <section class="flex flex-row items-start justify-around mb-8 text-sm text-left md:justify-end md:w-full md:text-lg">
+      <section class="my-8 ml-1 md:mr-20">
+        <h2 class="text-gray-100">Navigation</h2>
+        <ul class="text-gray-500 rounded list-reset" v-for="navItem in navItems" :key="navItem.title">
+          <li class="my-3">
+            <g-link :to="`${navItem.route}`">{{ navItem.title }}</g-link>
+          </li>
+        </ul>
       </section>
-      <section class="flex flex-row items-start justify-around mb-8 text-sm flex-nowrap">
-          <section class="my-8">
-            <h2 class="text-gray-100">Navigation</h2>
-            <ul class="text-gray-500 rounded list-reset" v-for="navItem in navItems" :key="navItem.title">
-              <li class="my-3">
-                <g-link :to="`${navItem.route}`">{{ navItem.title }}</g-link>
-              </li>
-            </ul>
-          </section>
-          <section class="mt-8">
-            <h2 class="text-gray-100">Our Menu</h2>
-            <ul class="text-gray-500 rounded list-reset" v-for="menuItem in menuItems" :key="menuItem.title">
-              <li class="my-3">
-                <g-link :to="`${menuItem.route}`">{{ menuItem.title }}</g-link>
-              </li>
-            </ul>
-          </section>
-        <section class="mt-8">
-          <h2 class="text-gray-100">Get in touch</h2>
-          <ul class="text-gray-500 rounded list-reset" v-for="contactItem in contactItems" :key="contactItem.title">
-            <li class="my-3">
-              <g-link :to="`${contactItem.route}`">{{ contactItem.title }}</g-link>
-            </li>
-          </ul>
-        </section>
+      <section class="mt-8 md:mr-16">
+        <h2 class="text-gray-100">Menu</h2>
+        <ul class="text-gray-500 rounded list-reset" v-for="menuItem in menuItems" :key="menuItem.title">
+          <li class="my-3">
+            <g-link :to="`${menuItem.route}`">{{ menuItem.title }}</g-link>
+          </li>
+        </ul>
       </section>
-    </footer>
-        <div class="py-2 mx-4 text-xs text-center border-solid sm:order-2">
-            <p class="mb-4">JAMstack Website by <span class="font-semibold hover:text-green-400">Chadd Web Design</span></p>
-            <p class="mb-2">Copyright &copy; {{ new Date().getFullYear() }} The Black Swan Restaurant & Wine Bar. All rights reserved.</p>
-        </div>
-      </div>
+      <section class="mt-8 mr-2 md:mr-16">
+        <h2 class="text-gray-100">Get in touch</h2>
+        <ul class="text-gray-500 rounded list-reset" v-for="contactItem in contactItems" :key="contactItem.title">
+          <li class="my-3">
+            <g-link :to="`${contactItem.route}`">{{ contactItem.title }}</g-link>
+          </li>
+        </ul>
+      </section>
+    </section>
+    <section class="py-2 mx-4 text-xs text-center border-solid md:order-2 md:text-left">
+      <p class="mb-4 md:ml-32">JAMstack Website by <span class="font-semibold hover:text-green-400">Chadd Web Design</span></p>
+      <p class="mb-2 md:ml-8 md:mb-8">Copyright &copy; {{ new Date().getFullYear() }} The Black Swan Restaurant & Wine Bar. All rights reserved.</p>
+    </section>
+  </footer>
 </template>
 
 <script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,68 +1,77 @@
 <template>
   <Layout>
-    <section id="hero-section" class="relative text-gray-100 bg-gray-900" v-for="edge in $page.hero.edges" :key="edge.node.id">
-      <video class="w-full h-full bg-gray-900 opacity-50" muted autoplay loop :src="edge.node.heroVideo.file.url" />
-      <section class="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full">
-        <h1 class="mx-8 font-bold text-center uppercase">{{ edge.node.heroTextHeading }}</h1>
-        <h3 class="mx-4 my-4 text-sm font-semibold text-center">{{ edge.node.heroTextSubHeading }}</h3>
-        <g-link to="/reservetable"><button class="px-4 py-3 text-sm font-semibold text-gray-100 bg-red-700 shadow-md rounded-xl focus:outline-none">Make A Reservation</button></g-link>
+    <section class="text-gray-900 bg-gray-100 md:text-xl">
+      <section id="hero-section" class="relative text-gray-100 bg-gray-900" v-for="edge in $page.hero.edges" :key="edge.node.id">
+        <video class="w-full h-full bg-gray-900 opacity-50" muted autoplay loop :src="edge.node.heroVideo.file.url" />
+        <section class="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full">
+          <h1 class="mx-8 font-bold text-center uppercase">{{ edge.node.heroTextHeading }}</h1>
+          <h3 class="mx-4 my-4 font-semibold text-center">{{ edge.node.heroTextSubHeading }}</h3>
+          <g-link to="/reservetable"><button class="px-4 py-3 font-semibold text-gray-100 bg-red-700 shadow-md rounded-xl focus:outline-none">Make A Reservation</button></g-link>
+        </section>
       </section>
-    </section>
-      <section id="overview-section" class="flex flex-col items-center text-gray-900 bg-gray-100 justify-items">
-        <h2 class="mt-8 text-lg font-bold">The Black Swan Restaurant</h2>
-        <h3 class="text-sm font-semibold">Fine Dining Restaurant in Oudtshoorn</h3>
-        <g-image class="mt-8 mb-4 rounded" src="../../static/img/patrons-of-the-black-swan-restaurant-in-oudtshoon.webp" alt="Patrons enjoying some drinks at The Black Swan Restaurant in Oudtshoorn" />
-        <p class="mx-6">Located in the heart of Oudtshoorn, this a lá carte hotspot is a popular restaurant with locals and visitors alike, and is abuzz with activity throughout the year. During the KKNK in March, the Black Swan is ground zero for South Africa’s biggest celebrities who hang out and perform live for the crowds.</p>
-        <g-link class="my-8 text-sm text-red-600" to="/about"><p>Find out more About Us...</p></g-link>
-      </section>
-      <section id="why-us-section" class="py-8 bg-gray-200">
-        <h2 class="my-8 text-lg text-center">Why we're unique</h2>
-        <section class="flex flex-col items-center justify-center mt-8">
-          <section class="flex flex-col items-center justify-center">
-            <h3 class="text-sm font-semibold">Wine Bar</h3>
-            <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
-          </section>
-          <section class="mt-8 mb-4">
-            <g-image src="../../static/img/the-black-swan-premium-wine.webp" alt="" />
-          </section>
-          <section class="mt-4">
-            <p class="mx-8">Our wine bar is the perfect spot to relax and enjoy an evening out with a loved one or friends. Enjoy the beautiful Klein Karoo while sipping on whites and reds from all around the country, our wine bar lets you unwind in style.</p>
+        <section id="overview-section" class="flex flex-col items-center text-gray-900 bg-gray-100 justify-items">
+          <h2 class="mx-8 mt-8 text-xl font-bold text-yellow-700">The Black Swan Restaurant</h2>
+          <h3 class="font-semibold text-center">Fine Dining Restaurant in Oudtshoorn</h3>
+          <section class="md:flex md:items-center md:justify-around md:py-20">
+            <section class="md:ml-8 md:w-1/2">
+              <g-image width="450" height="300" class="mt-8 mb-4 rounded" src="../../static/img/patrons-of-the-black-swan-restaurant-in-oudtshoon.webp" alt="Patrons enjoying some drinks at The Black Swan Restaurant in Oudtshoorn" />
+            </section>
+            <section class="md:w-1/2">
+              <p class="mx-6">Located in the heart of Oudtshoorn, this a lá carte hotspot is a popular restaurant with locals and visitors alike, and is abuzz with activity throughout the year. During the KKNK in March, the Black Swan is ground zero for South Africa’s biggest celebrities who hang out and perform live for the crowds.</p>
+              <g-link to="/about"><p class="my-8 text-xs font-semibold text-center text-red-600 uppercase">Find out more About Us...</p></g-link>
+            </section>
           </section>
         </section>
-        <section class="flex flex-col items-center justify-center mt-8">
-          <section class="flex flex-col items-center justify-center">
-            <h3 class="text-sm font-semibold">Our Menu</h3>
-            <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
+        <section id="why-us-section" class="py-8 bg-gray-200">
+          <h2 class="my-8 text-xl font-bold text-center text-yellow-700 uppercase">Why we're unique</h2>
+          <section class="md:flex md:flex-row md:flex-wrap md:items-center md:justify-center">
+            <section class="flex flex-col items-center justify-center mt-8 md:w-3/6">
+              <section class="flex flex-col items-center justify-center">
+                <h3 class="font-semibold">Wine Bar</h3>
+                <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
+                <section class="mt-8 mb-4">
+                  <g-image width="450" height="250" src="../../static/img/the-black-swan-premium-wine.webp" alt="" />
+                </section>
+                <section class="mt-4">
+                  <p class="mx-8 md:mx-16">Our wine bar is the perfect spot to relax and enjoy an evening out with a loved one or friends. Enjoy the beautiful Klein Karoo while sipping on whites and reds from all around the country, our wine bar lets you unwind in style.</p>
+                </section>
+              </section>
+            </section>
+            <section class="flex flex-col items-center justify-center mt-8 md:w-3/6">
+            <section class="flex flex-col items-center justify-center md:mt-12">
+              <h3 class="font-semibold">Our Menu</h3>
+              <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
+            </section>
+            <section class="mt-8 mb-4">
+              <g-image width="450" height="250" src="../../static/img/the-black-swan-beef-cuisine.webp" alt="" />
+            </section>
+            <section class="mt-4">
+              <p class="mx-8 md:mx-16">As the primo fine dining restaurant in Oudtshoorn we wow your tastebuds with mouthwatering dishes and decadent desserts. Come enjoy a traditional ostrich steak and find out why we were voted in the Top 20 Klein Karoo Smulpaap awards.</p>
+            </section>
           </section>
-          <section class="mt-8 mb-4">
-            <g-image src="../../static/img/the-black-swan-beef-cuisine.webp" alt="" />
+          <section class="flex flex-col items-center justify-center mt-8 md:w-3/6">
+            <section class="flex flex-col items-center justify-center">
+              <h3 class="font-semibold">Shuttle Service</h3>
+              <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
+            </section>
+            <section class="mt-8 mb-4">
+                <g-image width="450" height="250" src="../../static/img/the-black-swan-shuttle-services.webp" alt="" />
+            </section>
+            <section class="mt-4">
+              <p class="mx-8 md:mx-16">Our shuttle service is free and will take you to and from the restaurant. The result? You can enjoy a glass of wine without worrying about finding your way home afterwards.</p>
+            </section>
           </section>
-          <section class="mt-4">
-            <p class="mx-8">As the primo fine dining restaurant in Oudtshoorn we wow your tastebuds with mouthwatering dishes and decadent desserts. Come enjoy a traditional ostrich steak and find out why we were voted in the Top 20 Klein Karoo Smulpaap awards.</p>
-          </section>
-        </section>
-        <section class="flex flex-col items-center justify-center mt-8">
-          <section class="flex flex-col items-center justify-center">
-            <h3 class="text-sm font-semibold">Shuttle Service</h3>
-            <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
-          </section>
-          <section class="mt-8 mb-4">
-            <g-image src="../../static/img/the-black-swan-shuttle-services.webp" alt="" />
-          </section>
-          <section class="mt-4">
-            <p class="mx-8">Our shuttle service is free and will take you to and from the restaurant. The result? You can enjoy a glass of wine without worrying about finding your way home afterwards.</p>
-          </section>
-        </section>
-        <section class="flex flex-col items-center justify-center my-8">
-          <section class="flex flex-col items-center justify-center">
-          <h3 class="text-sm font-semibold">Mintt Street Café & Coffee Shop</h3>
-            <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
-          </section>
-          <section class="mt-8 mb-4">
-            <g-image src="../../static/img/the-black-swan-mintt-cafe.webp" alt="" />
-          </section>
-          <section class="mt-4">
-          <p class="mx-8">Join us at our daytime coffee shop, the Mintt Street Café, for an all-day breakfast and light lunch. Snack on a delicious camembert and fig sandwich, or treat yourself to a traditional ostrich burger alongside freshly hand-cut potato wedges.</p>
+          <section class="flex flex-col items-center justify-center my-8 md:w-3/6">
+            <section class="flex flex-col items-center justify-center md:mt-20">
+            <h3 class="font-semibold">Mintt Street Café & Coffee Shop</h3>
+              <div class="w-32 pb-4 border-b border-red-700 border-solid"></div>
+            </section>
+            <section class="mt-8 mb-4">
+              <g-image width="450" height="250" src="../../static/img/the-black-swan-mintt-cafe.webp" alt="" />
+            </section>
+            <section class="mt-4">
+              <p class="mx-8 md:mx-16">Join us at our daytime coffee shop, the Mintt Street Café, for an all-day breakfast and light lunch. Snack on a delicious camembert and fig sandwich, or treat yourself to a traditional ostrich burger alongside freshly hand-cut potato wedges.</p>
+            </section>
           </section>
         </section>
       </section>
@@ -72,12 +81,12 @@
         <h2></h2>
       </section>
       <section id="latest-articles" class="flex flex-col items-center text-gray-900 bg-gray-100 justify-items">
-        <h2 class="mt-8 text-lg font-bold">Latest Articles</h2>
+        <h2 class="mt-8 text-xl font-bold text-yellow-700 uppercase">Latest Articles</h2>
         <ul v-for="edge in $page.post.edges" :key="edge.node.id">
           <li class="flex flex-col items-center justify-center">
-            <h3 class="mt-8 text-sm font-semibold">{{ edge.node.title }}</h3>
-            <h4 class="mb-8 text-xs">by {{ edge.node.author.name }}</h4>
-            <g-image class="md:rounded" :src="edge.node.image.file.url" alt="" />
+            <h3 class="mt-8 font-semibold">{{ edge.node.title }}</h3>
+            <h4 class="mb-8 text-sm">by {{ edge.node.author.name }}</h4>
+            <g-image width="450" height="250" class="md:rounded" :src="edge.node.image.file.url" alt="" />
             <g-link :to="`/post/${edge.node.slug}`"><button class="px-5 py-3 my-8 font-semibold text-gray-100 bg-red-700 shadow-md rounded-xl focus:outline-none">Read More</button></g-link>
           </li>
         </ul>
@@ -85,9 +94,9 @@
       <section id="testimonials-section">
         <h2></h2>
       </section>
-      <section id="contact-us" class="flex flex-col items-center justify-center py-8">
-        <h2 class="my-4 uppercase">Need to get in touch?</h2>
-        <h3 class="mb-8">Head on over to our Contact Page <br> to get all our details</h3>
+      <section id="contact-us" class="flex flex-col items-center justify-center py-8 text-center bg-gray-200">
+        <h2 class="mx-8 my-4 text-xl font-bold text-yellow-700 uppercase">Need to get in touch?</h2>
+        <h3 class="mx-4 mb-8">Head on over to our Contact Page to get all our details</h3>
         <g-link to="/contact" ><button class="px-5 py-3 font-semibold text-gray-100 bg-red-700 shadow-md rounded-xl focus:outline-none">Contact Us</button></g-link>
       </section>
       <section id="affiliated-section">
@@ -99,6 +108,7 @@
           <h2></h2>
         </section>
       </section>
+    </section>
   </Layout>
 </template>
 
@@ -143,7 +153,13 @@ query Post {
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Fine Dining Restaurant in Oudtshoorn | The Black Swan'
   },
+  meta: [
+    {
+      name: 'description',
+      content: 'Located in the heart of Oudtshoorn, this hotspot is a popular restaurant with locals and visitors alike, and is abuzz with activity throughout the year.'
+    }
+  ]
 }
 </script>
